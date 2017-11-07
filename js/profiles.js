6372bfca1e34d9cb28dc970d8597515f70bc98af
Cristina Ahamad <cahamad@ucsd.edu>
2017-11-06 23:52:12 -0800

    new profile form

--- js/profiles.js
+++ js/profiles.js
@@ -18,7 +18,7 @@ function getFormInfo() {
       //var dislikes;
       //var likes;
 
-    arrayOfProfiles.push(entry);
+    arrayOfProfiles.push(JSON.stringify(entry));
     localStorage.setItem("profiles-list", JSON.stringify(arrayOfProfiles));
   //for (var key in entry) {
     //var person = JSON.parse(entry[key]);
