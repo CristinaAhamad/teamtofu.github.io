21c0b3c284e414dc0f8770b3aa4d6afe5401905c
Cristina Ahamad <cahamad@ucsd.edu>
2017-11-14 15:42:54 -0800

    profiles edit

--- js/profiles.js
+++ js/profiles.js
@@ -103,7 +103,7 @@ function addToProfilePage() {
   var currentLast = person[1];
   var fullName = currentFirst+" "+currentLast;
 
-  $("#profilespg").append('<a style="display:block" href="/Users/tinaahamad/Desktop/CSE170 Project/teamtofu/profile.html"> <img src="img/jrdo.png" style="width:40%;height:40%;border:0;"> <font class="prof-name" size="5">'+ fullName+'</font> </a>');
+  $("#profilespg").append('<a style="display:block" href="https://cristinaahamad.github.io/teamtofu/profile.html"> <img src="img/jrdo.png" style="width:40%;height:40%;border:0;"> <font class="prof-name" size="5">'+ fullName+'</font> </a>');
 
-  window.location ="/Users/tinaahamad/Desktop/CSE170 Project/teamtofu/profiles-page.html";
+  window.location ="https://cristinaahamad.github.io/teamtofu/profiles-page.html";
 };
