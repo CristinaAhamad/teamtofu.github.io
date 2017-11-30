6e16636211b96d4a87d6a6c6d4a9a590196d593b
Cristina Ahamad <cahamad@ucsd.edu>
2017-11-30 00:11:31 -0800

    analytics

--- js/filters.js
+++
@@ -1,9 +0,0 @@
-var source = $('#filter-template').html();
-var template = Handlebars.compile(source);
-var profiles = JSON.parse(localStorage.getItem('profiles-list'));
-var parentDiv = $('#filters');
-
-$(document).ready(function() {
-
-  parentDiv.append(html);
-});
